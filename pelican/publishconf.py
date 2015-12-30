cde93115604ddc5fb10e9dd331093b70b8c9a47c
nashnash <40323213@gm.nfu.edu.tw>
2015-12-30 16:03:59 +0800

    151230

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -26,8 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Python', 'http://python.org/'),('個人倉儲網站', 'http://nashnash.github.io/40323213cphw/'),('個人github倉儲網站', 'https://github.com/nashnash/40323213cphw/tree/gh-pages'),('Group9倉儲網站', 'http://2015fallhw.github.io/2015fallcadpb/category/g9.html'))
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
